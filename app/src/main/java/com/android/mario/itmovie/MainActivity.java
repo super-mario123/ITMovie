84bb2d22b55cdfe23cdad7d3b18f390a11628a66
super-mario123 <nameicpico@gmail.com>
2015-07-08 21:00:09 +0800

    first commit

--- app/src/main/java/com/android/mario/itmovie/MainActivity.java
+++ app/src/main/java/com/android/mario/itmovie/MainActivity.java
@@ -14,6 +14,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     private ImageBtn mImageBtnOK;
     private ImageBtn mImageBtnERROR;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
