e50975d1bf75e9d7fa4333a85aa2cf5b783eb36f
super-mario123 <nameicpico@gmail.com>
2015-07-08 20:48:50 +0800

    first commit

---
+++ app/src/main/java/com/android/mario/itmovie/MainActivity.java
@@ -0,0 +1,65 @@
+package com.android.mario.itmovie;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Toast;
+
+import com.android.mario.itmovie.com.android.mario.itmovie.views.ImageBtn;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private ImageBtn mImageBtnOK;
+    private ImageBtn mImageBtnERROR;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mImageBtnOK = (ImageBtn) findViewById(R.id.image_smile_btn);
+        mImageBtnOK.setTextViewText("确定");
+        mImageBtnOK.setImageRes(R.mipmap.smile_black);
+        mImageBtnERROR = (ImageBtn) findViewById(R.id.image_sad_btn);
+        mImageBtnERROR.setTextViewText("取消");
+        mImageBtnERROR.setImageRes(R.mipmap.sad_black);
+        mImageBtnOK.setOnClickListener(this);
+        mImageBtnERROR.setOnClickListener(this);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.image_smile_btn:
+                Toast.makeText(this,"确定",Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.image_sad_btn:
+                Toast.makeText(this,"取消",Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+}
