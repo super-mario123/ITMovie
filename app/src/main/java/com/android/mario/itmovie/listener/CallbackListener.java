cebd5d3c5cc09557276115a6fcb0b85782ec672a
super-mario123 <nameicpico@gmail.com>
2015-07-09 17:15:33 +0800

    callback commit

--- app/src/main/java/com/android/mario/itmovie/listener/CallbackListener.java
+++ app/src/main/java/com/android/mario/itmovie/listener/CallbackListener.java
@@ -3,5 +3,18 @@ package com.android.mario.itmovie.listener;
 /**
  * Created by Administrator on 2015/7/9.
  */
-public interface CallbackListener {
+public interface CallbackListener<T> {
+
+    /**
+     * 请求响应结果成功时调用
+     * @param data 返回的数据
+     */
+    public void onSuccess(T data);
+
+    /**
+     * 请求响应结果失败时调用
+     * @param errorEvent 错误码
+     * @param message 错误信息
+     */
+    public void onFailure(String errorEvent, String message);
 }
